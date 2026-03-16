1c142939ca92208296b811e56f1ba45d99e550ed
Dashyla <darya.kolupaeva5654@mail.ru>
2026-03-16 16:13:45 +0700

    added  file encryption

--- main.cpp
+++ main.cpp
@@ -1,24 +1,87 @@
 #include <QCoreApplication>
 #include <QTextStream>
-#include "directorywalker.h"
-/*
-int main(int argc, char *argv[]){
-    QCoreApplication a(argc, argv);
-
-    QString startPath = "C:/Users/darya/Desktop/Combez/7_semester/Number_Theory_Methods_in_Cryptography";    
-    QTextStream out(stdout);
-
-    DirectoryWalker walker;
-
-    walker.listDirsRecursively(startPath, out);
-
-    return 0;
-}
-    */
+#include <QDir>
 #include <iostream>
+#include "directorywalker.h"
+#include "encryptor.h"
 #include <openssl/crypto.h>
 
+using namespace std;
+
+int main(int argc, char *argv[]){
+    setlocale(LC_ALL, "Russian");
+    QCoreApplication a(argc, argv);
+    Encryptor encryptor;
+    QTextStream out(stdout);
+
+    while (true) {
+        cout << "Выберите действие:" << endl;
+        cout << "1. Зашифровать файл" << endl;
+        cout << "2. Расшифровать файл" << endl;
+        cout << "0. Выход" << endl;
+        cout << "Ваш выбор: ";
+        
+        int choice;
+        cin >> choice;
+        cin.ignore();
+        
+        if (choice == 0) {
+            cout << "Программа завершена." << endl;
+            break;
+        }
+        
+        string filePathStr;
+        string passwordStr;
+        
+        switch (choice) {
+            case 1:
+                cout << "ШИФРОВАНИЕ" << endl;
+                
+                cout << "Введите путь к файлу для шифрования: ";
+                std::getline(cin, filePathStr);
+                
+                cout << "Введите пароль: ";
+                std::getline(cin, passwordStr);
+                
+                if (encryptor.encryptFile(
+                    QString::fromStdString(filePathStr), 
+                    QString::fromStdString(passwordStr))) {
+                    cout << "Файл успешно зашифрован!" << endl;
+                } else {
+                    cout << "Ошибка при шифровании файла!" << endl;
+                }
+                break;
+                
+            case 2: 
+                cout << "ДЕШИФРОВАНИЕ" << endl;
+                
+                cout << "Введите путь к файлу для дешифрования: ";
+                std::getline(cin, filePathStr);
+                
+                cout << "Введите пароль: ";
+                std::getline(cin, passwordStr);
+                
+                // Выполняем дешифрование
+                if (encryptor.decryptFile(
+                    QString::fromStdString(filePathStr), 
+                    QString::fromStdString(passwordStr))) {
+                    cout << "Файл успешно дешифрован!" << endl;
+                } else {
+                    cout << "Ошибка при дешифровании файла!" << endl;
+                }
+                break;
+            default:
+                cout << "Неверный выбор!" << endl;
+                break;
+        }
+    }
+    
+    return 0;
+}
+/*    
 int main() {
+    setlocale(LC_ALL, "Russian");
     std::cout << "OpenSSL version: " << OpenSSL_version(OPENSSL_VERSION) << std::endl;
     return 0;
 }
+*/
