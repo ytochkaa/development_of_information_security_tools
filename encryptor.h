5f1920f0872a9a89be98b9e3ed998e89030a2d42
Dashyla <darya.kolupaeva5654@mail.ru>
2026-03-23 16:23:57 +0700

    ban re-encryption of data

--- encryptor.h
+++ encryptor.h
@@ -1,18 +1,22 @@
 #include <QString>
 #include <QByteArray>
 
+static const int KEY_SIZE = 32;  
+static const int NONCE_SIZE = 12;
+static const int TAG_SIZE = 16;
+const int HASH_SIZE = 32;
+
 class Encryptor {
-private:
-    static const int KEY_SIZE = 32;  
-    static const int NONCE_SIZE = 12;
-    static const int TAG_SIZE = 16;
-    
 public:
     Encryptor();
+    static bool isFileEncrypted(const QString &filePath);
     
     bool encryptFile(const QString &filePath, const QString &password);
     bool decryptFile(const QString &filePath, const QString &password);
     
 private:
     QByteArray deriveKeyFromPassword(const QString &password);
+    static QByteArray calculateFileHash(const QString &filePath);
+    static bool verifyFileHash(const QString &filePath, const QByteArray &expectedHash);
+    
 };
