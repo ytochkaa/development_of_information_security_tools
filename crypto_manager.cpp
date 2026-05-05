20bcccddb8f7c6331b77524bdcfc11fe06087bb8
Dashyla <darya.kolupaeva5654@mail.ru>
2026-05-05 15:24:06 +0700

    added Inline directory traversal to CryptoManager

--- crypto_manager.cpp
+++ crypto_manager.cpp
@@ -1,4 +1,7 @@
 #include "crypto_manager.h"
+#include <QDir>
+#include <QDirIterator>
+#include <iostream>
 
 CryptoManager::CryptoManager() {}
 
@@ -13,4 +16,44 @@ bool CryptoManager::encrypt(const QString &filePath, const QString &password) {
 
 bool CryptoManager::decrypt(const QString &filePath, const QString &password) {
     return decryptor.decryptFile(filePath, password);
+}
+
+bool CryptoManager::encryptDirectory(const QString &dirPath, const QString &password) {
+    QDir dir(dirPath);
+    if (!dir.exists()) {
+        std::cout << "Директория не найдена: " << dirPath.toStdString() << std::endl;
+        return false;
+    }
+
+    QDirIterator it(dirPath, QDir::Files | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
+    bool success = true;
+    while (it.hasNext()) {
+        QString filePath = it.next();
+        if (!encryptor.isFileEncrypted(filePath)) {
+            if (!encryptor.encryptFile(filePath, password)) {
+                success = false;
+            }
+        }
+    }
+    return success;
+}
+
+bool CryptoManager::decryptDirectory(const QString &dirPath, const QString &password) {
+    QDir dir(dirPath);
+    if (!dir.exists()) {
+        std::cout << "Директория не найдена: " << dirPath.toStdString() << std::endl;
+        return false;
+    }
+
+    QDirIterator it(dirPath, QDir::Files | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
+    bool success = true;
+    while (it.hasNext()) {
+        QString filePath = it.next();
+        if (encryptor.isFileEncrypted(filePath)) {
+            if (!decryptor.decryptFile(filePath, password)) {
+                success = false;
+            }
+        }
+    }
+    return success;
 }
--- crypto_manager.h
+++ crypto_manager.h
@@ -7,6 +7,8 @@ public:
 
     bool encrypt(const QString &filePath, const QString &password);
     bool decrypt(const QString &filePath, const QString &password);
+    bool encryptDirectory(const QString &dirPath, const QString &password);
+    bool decryptDirectory(const QString &dirPath, const QString &password);
 
 private:
     CryptoManager();
